b17cc01f6b95aa1cb7b0adab5eaa4c9ea4f8700c
sirapaop <sirapaop2545@gmail.com>
2023-02-18 13:46:06 +0700

    simulation delete subj

--- src/gpa_cal.js
+++ src/gpa_cal.js
@@ -5,10 +5,10 @@ function gpa_cal(){
 
 
 function addsubj(){ 
-	var table = document.getElementById('grade_table');
-	var rowCount = table.rows.length;
-	var cellCount = table.rows[0].cells.length; 
-	var row = table.insertRow(rowCount);
+	var table = document.getElementById('grade_table');     //call table
+	var rowCount = table.rows.length;                       // count tr
+	var cellCount = table.rows[0].cells.length;             // return first td
+	var row = table.insertRow(rowCount);                    // create new row
 	for(var i =0; i <= cellCount; i++){
 		var cell = 'cell'+i;
 		cell = row.insertCell(i);
@@ -26,3 +26,14 @@ function addsubj(){
 	}
 }
 
+function deletesubj(){
+	var table = document.getElementById('grade_table');
+	var rowCount = table.rows.length;
+	if(rowCount > '2'){
+		var row = table.deleteRow(rowCount-1);
+		rowCount--;
+	}
+	else{
+		alert('There should be atleast one subject to calculate :>');
+	}
+}
