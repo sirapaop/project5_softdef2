d0973421a9733f19a1bd83bac489f1142c49f807
sirapaop <sirapaop2545@gmail.com>
2023-02-18 13:34:38 +0700

    add row simmulation grade

--- src/gpa_cal.js
+++ src/gpa_cal.js
@@ -2,3 +2,27 @@ function gpa_cal(){
     var x = 3 / 2
     document.getElementById("demo").innerHTML = x;
 }
+
+
+function addsubj(){ 
+	var table = document.getElementById('grade_table');
+	var rowCount = table.rows.length;
+	var cellCount = table.rows[0].cells.length; 
+	var row = table.insertRow(rowCount);
+	for(var i =0; i <= cellCount; i++){
+		var cell = 'cell'+i;
+		cell = row.insertCell(i);
+		var copycel = document.getElementById('col'+i).innerHTML;
+		cell.innerHTML=copycel;
+		if(i == 3){ 
+			var radioinput = document.getElementById('col3').getElementsByTagName('input'); 
+			for(var j = 0; j <= radioinput.length; j++) { 
+				if(radioinput[j].type == 'radio') { 
+					var rownum = rowCount;
+					radioinput[j].name = 'gender['+rownum+']';
+				}
+			}
+		}
+	}
+}
+
